fd3a9fa9ae90005c0bc0cb849d3ef4610b4f2f1a
jegelber <gelber_j1@denison.edu>
2020-07-20 12:46:19 -0400

    Update grading.py

--- algorithms/implementation/grading.py
+++ algorithms/implementation/grading.py
@@ -4,13 +4,11 @@
 def gradingStudents(grades):
     ret = []
     for g in grades:
-        if g < 38:
+        if g < 38 or g%5 < 3:
             ret.append(g)
         else:
             if (g+1)%5 == 0:
                 ret.append(g+1)
-            elif (g+2)%5 == 0:
+            else: # g+2 % 5 == 0
                 ret.append(g+2)
-            else:
-                ret.append(g)
     return ret
