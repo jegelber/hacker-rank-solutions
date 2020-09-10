cf2e469dff98d40dbf7ffccdf59efd54984d4289
jegelber <gelber_j1@denison.edu>
2020-09-10 13:40:28 -0400

    Created the-birthday-bar.py

---
+++ algorithms/implementation/the-birthday-bar.py
@@ -0,0 +1,9 @@
+# Problem: https://www.hackerrank.com/challenges/the-birthday-bar/problem
+# Score: 10/10
+
+def birthday(s, d, m):
+    count = 0
+    for i in range(len(s) - m + 1):
+        if sum(s[i : i + m]) == d:
+            count = count + 1
+    return count
